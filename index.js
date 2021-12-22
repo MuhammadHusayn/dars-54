f3c440d3de14a17f457a77a87920cb9833b6fa14
MuhammadHusayn Olimjonov <olimjonovm007@gmail.com>
2021-12-22 14:30:24 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+	router path
+		* /users
+		* /hello.txt
+		* /ab?cd
+		* /ab+cd
+		* /ab*cd
+		* /ab(cd)?e
+		* /.*fly$/
+		* /users/:userId
+
+	middleware | route handler
+		* build-in middleware
+		* custom middleware
+		* third-part middleware
+
+	route control
+		express.Router()
+		app.route()
+		app.all()
+		app.use()
+*/
